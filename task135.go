4da70cf2708a4bc444b0a20b7bcbcfa6f49009d2
Sergey Mayorov <s.mayorov@automacon.ru>
2024-06-23 01:19:43 +0300

    init v1.1.0

--- task135.go
+++ task135.go
@@ -1,11 +1,12 @@
 package task135
 
-// v1.0.0
+// v1.1.0
 import (
 	"bufio"
 	"encoding/json"
 	"fmt"
 	"os"
+	"sort"
 )
 
 type Patient struct {
@@ -13,6 +14,12 @@ type Patient struct {
 	Age   int    `json:"age"`
 	Email string `json:"email"`
 }
+
+type ByAge []Patient
+
+func (a ByAge) Len() int           { return len(a) }
+func (a ByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByAge) Less(i, j int) bool { return a[i].Age < a[j].Age }
 
 func Do(inputPath, outputPath string) error {
 	data, err := os.Open(inputPath)
@@ -34,6 +41,8 @@ func Do(inputPath, outputPath string) error {
 		patients = append(patients, patient)
 	}
 	fmt.Println(patients)
+
+	sort.Sort(ByAge(patients))
 
 	jsonData, err := json.MarshalIndent(patients, "", "  ")
 	if err != nil {
